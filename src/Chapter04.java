6503fdbf3665ef0c24b0ebf07a5b7f57f3daf120
kztn0112 <ngwn1995@gmail.com>
2021-09-06 01:02:35 +0900

    [学習]Java基礎4

---
+++ src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		 String name= "kztn0112";
+	     System.out.println(name);
+	     name= "にゃん";
+	     System.out.println(name);
+	     final int LUCKY_NUMBER= 1;
+	     System.out.println(LUCKY_NUMBER);
+    }
+}
