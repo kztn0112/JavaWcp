d50130ff4313596276547b0a68c0fc7edc7180c1
kztn0112 <ngwn1995@gmail.com>
2021-09-10 00:36:48 +0900

    [学習]Java基礎5

---
+++ src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 9223372036854775807L;
+		double double1 = 1234567.89;
+		boolean boolean1 = true;
+	    System.out.println(long1);
+	    System.out.println(double1);
+	    System.out.println(boolean1);
+	     
+	     Float float1 = 1234567.89f;
+	     Integer integer1 = 1;
+	     Character character1 = 'あ';
+	     System.out.println(float1);
+	     System.out.println(integer1);
+	     System.out.println(character1);
+	     
+	     int[] array1 = { 1, 5, 10, 20, 50 };
+	     System.out.println(array1[3]);
+	}
+}
