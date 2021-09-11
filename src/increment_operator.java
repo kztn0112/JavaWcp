b36fa14fd488eacba0f912aaf04363761469e9ec
kztn0112 <ngwn1995@gmail.com>
2021-09-11 23:36:54 +0900

    [学習]Java基礎6

---
+++ src/increment_operator.java
@@ -0,0 +1,12 @@
+
+public class increment_operator {
+	public static void main(String[] args) {
+		int a;
+		a = 10;
+		a++;  // a = 11
+		System.out.println(a);
+		a = 10;
+		a--;  // a = 9
+		System.out.println(a);
+	}
+}
