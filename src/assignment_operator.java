2b8b2bed6361faa2623241fe39813d4d1d2935f1
kztn0112 <ngwn1995@gmail.com>
2021-09-11 23:53:52 +0900

    [学習]Java基礎6

---
+++ src/assignment_operator.java
@@ -0,0 +1,18 @@
+
+public class assignment_operator {
+	public static void main(String[] args) {
+		int a;
+		a = 10;  // a = 10：aに10を代入
+		System.out.println(a);
+		a += 5;  // a = 15：aにa+5の結果を代入
+		System.out.println(a);
+		a -= 3;  // a = 12：aにa-3の結果を代入
+		System.out.println(a);
+		a *= 4;  // a = 48：aにa*4の結果を代入
+		System.out.println(a);
+		a /= 2;  // a = 24：aにa/2の結果を代入
+		System.out.println(a);
+		a %= 7;  // a = 3 ：aにa%7の結果を代入
+		System.out.println(a);
+	}
+}
