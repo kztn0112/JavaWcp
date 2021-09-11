94fda706a73409d5825267fcc735b53301c254d9
kztn0112 <ngwn1995@gmail.com>
2021-09-11 23:24:48 +0900

    [学習]Java基礎6

---
+++ src/arithmetic_operator.java
@@ -0,0 +1,13 @@
+
+public class arithmetic_operator {
+	public static void main(String[] args) {
+		int a = 15;
+		int c;
+		c = a + 3;  // c = 18
+		c = a - 5;  // c = 10
+		c = a * 2;  // c = 20
+		c = a / 6;  // c = 2
+		c = a % 4;  // c = 3
+		System.out.println(c);
+	}
+}
