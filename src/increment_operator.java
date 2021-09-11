37ab42cc62268281919854ccd046ef84d37e0f82
kztn0112 <ngwn1995@gmail.com>
2021-09-11 23:46:39 +0900

    [学習]Java基礎6

--- src/increment_operator.java
+++ src/increment_operator.java
@@ -8,5 +8,19 @@ public class increment_operator {
 		a = 10;
 		a--;  // a = 9
 		System.out.println(a);
+		
+		//前置と後置
+		int c;
+		a = 5;
+		c = ++a;  // a をインクリメントした後に、a が c に代入される
+		// それぞれの値は、a が「6」で c も「6」
+		System.out.println(a);
+		System.out.println(c);
+
+		a = 5;
+		c = a++;  // a を c に代入した後に、a がインクリメントされる
+		// それぞれの値は、a が「6」で c が「5」
+		System.out.println(a);
+		System.out.println(c);
 	}
 }
