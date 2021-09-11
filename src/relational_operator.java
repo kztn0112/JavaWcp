7bc94d80a607e831c992cdb5c66213313e3f2840
kztn0112 <ngwn1995@gmail.com>
2021-09-11 23:57:08 +0900

    [学習]Java基礎6

---
+++ src/relational_operator.java
@@ -0,0 +1,20 @@
+
+public class relational_operator {
+	public static void main(String[] args) {
+		int a = 5;
+		boolean c;
+
+		c = a < 5;   // c = false
+		System.out.println(c);
+		c = a <= 5;  // c = true
+		System.out.println(c);
+		c = a > 4;   // c = true
+		System.out.println(c);
+		c = a >= 6;  // c = false
+		System.out.println(c);
+		c = a == 5;  // c = true
+		System.out.println(c);
+		c = a != 5;  // c = false
+		System.out.println(c);
+	}
+}
