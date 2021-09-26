33ec4f3e0b6b62687da8ebe8b76911af163c86be
kztn0112 <ngwn1995@gmail.com>
2021-09-26 22:21:10 +0900

    [学習]Java基礎12

--- src/capsule/human/HumanCapsule.java
+++ src/capsule/human/HumanCapsule.java
@@ -1,6 +1,7 @@
 package capsule.human;
 
 public class HumanCapsule {
+	// privateのアクセス修飾子を設定して外部から隠蔽
 	private String name;
     private int age;
 
@@ -8,15 +9,16 @@ public class HumanCapsule {
         this.name = name;
         this.age = age;
     }
-
+ // nameのgetterメソッド
     public String getName() {
         return this.name;
     }
-
+    // nameのsetterメソッド
     public void setName(String name) {
         this.name = name;
     }
-
+ // ageのgetterメソッド
+    // ageは外部から変更禁止なので、setterメソッドは作成しない
     public int getAge() {
         return this.age;
     }
